6aa624cad6ddefa4d45c18298160353a6a471290
Chris Livingston <clivingston@gmail.com>
2021-09-30 08:42:06 -0700

    files added

---
+++ db/empTRK.sql
@@ -0,0 +1,9 @@
+SELECT `department`.`id`,
+    `department`.`name`
+FROM `employees`.`department`;
+SELECT `employee`.`id`,
+    `employee`.`first_name`,
+    `employee`.`last_name`,
+    `employee`.`role_id`,
+    `employee`.`manager_id`
+FROM `employees`.`employee`;
